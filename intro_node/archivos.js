e27fe06ea975a2616176472bc03ab5896053b408
César León Schaus <clsc_7@hotmail.com>
2019-05-20 23:36:50 -0300

    Archivos (Escritura) - Clase 21

--- intro_node/archivos.js
+++ intro_node/archivos.js
@@ -3,7 +3,8 @@ const fs = require('fs');
 // leer archivo (texto.txt) de manera sincronica
 /*archivo = fs.readFileSync("./texto.txt");
 console.log(archivo.toString());*/
-var archivo;
+
+/*var archivo;
 //fs.readFile("./texto.txt", (err, data)=>{ //callback asymc
 fs.readFile("./err.txt", (err, data)=>{ //callback asymc
     //console.log(data.toString());
@@ -11,5 +12,12 @@ fs.readFile("./err.txt", (err, data)=>{ //callback asymc
     console.log(err);
     console.log(archivo);
 });
+console.log(archivo);*/
 
-console.log(archivo);
+const archivo = fs.createWriteStream("escritura.txt",{
+    flags: 'r+',
+    start: 5
+});
+//archivo.write("Hola mundo de clsc!!!\n");
+archivo.write("abcdfgh");
+archivo.end();
