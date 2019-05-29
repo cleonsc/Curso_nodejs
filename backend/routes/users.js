c29b7e0963ea678b50d736d5f6a3bb02b715fef4
César León Schaus <cschaus@teceng-gaming.com.ar>
2019-05-29 14:44:38 -0300

    Rutas - Clase 34

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,8 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
+});
+router.get('/:usuario', function (req, res, next) { //los : sirve para que el parametro sea variable
+  //req -> peticion del cliente(navegador web)
+  //res -> respuesta que vamos a enviar al cliente
+  //next -> callback , la usa express para continuar con su ejecución
+  let usuario = req.params.usuario;
+  res.send('Usuario: '+usuario);
 });
 
 module.exports = router;
