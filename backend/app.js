8bfc66d1df3848e69f711b7a984efe3689ed6a8a
César León Schaus <cschaus@teceng-gaming.com.ar>
2019-05-29 15:44:45 -0300

    Sessiones - Clase 35

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var animalesRouter = require('./routes/animales');
 
+//Creación de app de express
 var app = express();
+
+app.use(session({
+  secret: 'app nodejs'
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- backend/routes/animales.js
+++ backend/routes/animales.js
@@ -1,7 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+var contadorVistas = function(req,res,next){
+    if (req.session.vistas == undefined) {
+        req.session.vistas = 0;
+    }
+    req.session.vistas++;
+    next();
+};
+
+router.use(contadorVistas);
+
 router.get('/',(req,res,next)=>{
-    res.send("Recurso de animales");
+    console.log(req.session);
+    res.send("Recurso de animales ha sido visto "+req.session.vistas+" veces");
 })
 module.exports = router;
