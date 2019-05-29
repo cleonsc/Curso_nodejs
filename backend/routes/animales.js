03cf798c01227e69f9f5c45f6f2489e00bfedb42
César León Schaus <cschaus@teceng-gaming.com.ar>
2019-05-29 17:35:47 -0300

    Manejo y validación de peticiones HTTP - Clase 36

--- backend/routes/animales.js
+++ backend/routes/animales.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-var contadorVistas = function(req,res,next){
+var contadorVistas = function (req, res, next) {
     if (req.session.vistas == undefined) {
         req.session.vistas = 0;
     }
@@ -11,8 +11,28 @@ var contadorVistas = function(req,res,next){
 
 router.use(contadorVistas);
 
-router.get('/',(req,res,next)=>{
+router.get('/', (req, res, next) => {
     console.log(req.session);
-    res.send("Recurso de animales ha sido visto "+req.session.vistas+" veces");
-})
+    res.send("Recurso de animales ha sido visto " + req.session.vistas + " veces");
+});
+
+// para probar esto se usa restClient de chrome
+//crear un animal
+router.post('/', (req, res, next) => {
+    if (!req.session.animales) {
+        req.session.animales = [];
+    }
+
+    let body = req.body;
+    //{nombre, especie}
+    if(req.get('content-type')=="application/json"){
+        console.log("Validado");
+    }else{
+        res.status(403).send("Error");
+    }
+
+    req.session.animales.push(req.body);
+    console.log(req.session);
+    res.send("La peticion ha sido recibida.");
+});
 module.exports = router;
