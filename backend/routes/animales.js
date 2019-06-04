0b0b5378d7976bb797ac168919e4e85e8a8e536d
César León Schaus <cschaus@teceng-gaming.com.ar>
2019-06-04 17:02:22 -0300

    Middlewares - Clase 37

--- backend/routes/animales.js
+++ backend/routes/animales.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
+//todas las peticiones deben ser del tipo application/json
+var validacion = function(req,res,next){
+    if(req.get('content-type')=="application/json"){
+        console.log("Validado");
+    }else{
+        res.status(403).send("Error");
+    }
+     next(); 
+}
+
+
 var contadorVistas = function (req, res, next) {
     if (req.session.vistas == undefined) {
         req.session.vistas = 0;
@@ -9,6 +20,7 @@ var contadorVistas = function (req, res, next) {
     next();
 };
 
+router.use(validacion);
 router.use(contadorVistas);
 
 router.get('/', (req, res, next) => {
@@ -25,11 +37,11 @@ router.post('/', (req, res, next) => {
 
     let body = req.body;
     //{nombre, especie}
-    if(req.get('content-type')=="application/json"){
+    /*if(req.get('content-type')=="application/json"){
         console.log("Validado");
     }else{
         res.status(403).send("Error");
-    }
+    }*/
 
     req.session.animales.push(req.body);
     console.log(req.session);
